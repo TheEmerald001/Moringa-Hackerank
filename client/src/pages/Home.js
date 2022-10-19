24a786c890fb32bfa77ff7c052fadb865e55f3f6
Maurice <mauricenganga41@gmail.com>
2022-10-19 12:43:17 +0300

    Create home file in the pages directory and import routes from react-router-dom to use in routing pages and components

---
+++ client/src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const Home = () => {
+  return <Container></Container>;
+};
+
+export default Home;
+
+const Container = styled.main``;
