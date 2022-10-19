ef7fd827bd44d881a2ec900204c7e15b7f4111d9
steve <austines811@gmail.com>
2022-10-19 18:19:41 +0300

    proofs models and modifies essential associations

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,9 @@
 class Student < ApplicationRecord
-    has_many :associations
-    has_many :tutors, through: :associations
+    # has_many :associations Did you mean assessments?
+    has_many :assessments
+    # has_many :tutors, through: :assessments little correction, we have a tutor from an invite, thus modest to find my tutor on the invites table
     has_many :invites
-    belongs_to :tutors
+    has_many :tutors, through: :invites
+    # belongs_to :tutors we already esatblished that they have many tutors via invites
     has_many :attempts
 end
--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -1,7 +1,9 @@
 class Tutor < ApplicationRecord
-    has_many :associations
-    has_many :students, through: :associations
-    has_many :students
+    # has_many : Did you mean assessments?
+    has_many :assessments, dependent: :destroy
+    # has_many :students, through: :associations Again, modest to search my invited students and index them on an assesment I gave, by looking through the invites table
+    
     has_many :invites
+    has_many :students, through: :invites
     has_many :attempts
 end
