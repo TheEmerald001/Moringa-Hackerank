cb50efc9e62dad677fff79db738cdfaa9182a7c4
steve <austines811@gmail.com>
2022-10-20 01:24:34 +0300

    fix naming convention and attributes name for proper serialization

--- app/serializers/katum_serializer.rb
+++
@@ -1,4 +0,0 @@
-class KatumSerializer < ActiveModel::Serializer
-  attributes :id, :question, :instructions
-  has_one :assessment
-end
--- db/migrate/20221019113041_create_kataas.rb
+++ db/migrate/20221019113041_create_kataas.rb
@@ -1,6 +1,6 @@
-class CreateKata < ActiveRecord::Migration[6.1]
+class CreateKataas < ActiveRecord::Migration[6.1]
   def change
-    create_table :kata do |t|
+    create_table :kataas do |t|
       t.references :assessment, null: false, foreign_key: true
       t.string :question
       t.string :instructions
