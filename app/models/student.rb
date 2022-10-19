3c6b575451741d56652fc3fa02b584954c265e47
michael kaburu <michael.kaburu@student.moringaschool.com>
2022-10-19 15:01:58 +0300

    add associations to Student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,6 @@
 class Student < ApplicationRecord
+    has_many :associations
+    has_many :tutors, through: :associations
+    has_many :invites
+    belongs_to :tutors
 end
--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -2,4 +2,5 @@ class Tutor < ApplicationRecord
     has_many :associations
     has_many :students, through: :associations
     has_many :students
+    has_many :invites
 end
