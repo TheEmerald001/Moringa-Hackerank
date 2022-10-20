c0ded03c2bed091dfc099b23bd7efd7d20bf0023
Maurice <mauricenganga41@gmail.com>
2022-10-20 05:59:37 +0300

    Install sass using npm and modify the width of the sidebar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,18 +1,18 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Assesments from "../pages/Assesments";
 import Home from "../pages/Home";
+import List from "../pages/List";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/">
-          <Route index element={<Home />} />
+          <Route index element={<List />} />
         </Route>
         <Route path="/assesments">
           <Route index element={<Assesments />} />
         </Route>
-
       </Routes>
     </Router>
   );
