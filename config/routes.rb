9ffd80fc5a4f5a82df68d081d6825467e051872a
michael kaburu <michael.kaburu@student.moringaschool.com>
2022-10-21 00:20:48 +0300

    create tutorprofile index,show,create action controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  resources :tutorprofiles, only:[:show,:create]
-  resources :studentprofiles,only:[:show,:create]
+  resources :tutorprofiles, only:[:idex,:show,:create]
+  resources :studentprofiles,only:[:index,:show,:create]
   resources :attempts,only:[:show,:create,:update]
   resources :invites,only:[:show,:create,:destroy]
   resources :mcqs
