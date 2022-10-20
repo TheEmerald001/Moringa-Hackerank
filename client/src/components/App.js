4600977d6af5e55843dfbe12f12b7e94f0211f21
Maurice <mauricenganga41@gmail.com>
2022-10-20 04:27:09 +0300

    Create List page for display list of all students sorted by scores and create helper diretory to add helper js methods

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
+import List from "../pages/List";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/">
-          <Route index element={<Home />} />
+          <Route index element={<List />} />
         </Route>
       </Routes>
     </Router>
