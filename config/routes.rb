97c1e4a95f27fe7c55ff92fb279f4081da61e30f
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-10-20 23:56:42 +0300

    chore: updated routes for mcqs, pros, katas, assessments and tutors.

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
 
   resources :attempts
   resources :invites
-  resources :mcqs
-  resources :pros
-  resources :kataas
-  resources :assessments
+  resources :mcqs, only: [:show, :create, :update, :destroy]
+  resources :pros, only: [:show, :create, :update, :destroy]
+  resources :kataas, only: [:show, :create, :update, :destroy]
+  resources :assessments, only: [:show, :create, :update, :destroy]
   resources :students
-  resources :tutors
+  resources :tutors, only: [:show, :create]
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
