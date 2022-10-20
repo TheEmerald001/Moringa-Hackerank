4c899faaac891f7ac4e424c5cc98e44a75970d78
Maurice <mauricenganga41@gmail.com>
2022-10-20 04:27:09 +0300

    Create List page for display list of all students sorted by scores and create helper diretory to add helper js methods

---
+++ client/src/pages/List.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+import DataTable from "../components/DataTable";
+
+const List = () => {
+  return (
+    <Container>
+      <DataTable />
+    </Container>
+  );
+};
+
+export default List;
+
+const Container = styled.main``;
