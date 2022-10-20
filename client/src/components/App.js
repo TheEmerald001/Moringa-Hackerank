c1e469ef2f2751fb7e0c703aad41774a2a55a627
Maurice <mauricenganga41@gmail.com>
2022-10-20 05:02:55 +0300

    Create a single page that will display a single student and their answers when the technical mentor selects on a student

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import List from "../pages/List";
+import Single from "../pages/Single";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/">
-          <Route index element={<List />} />
+          <Route index element={<Single />} />
         </Route>
       </Routes>
     </Router>
