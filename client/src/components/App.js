078b8ac68537d32d1e01daf1fb3d7874602bd055
Maurice <mauricenganga41@gmail.com>
2022-10-21 07:39:09 +0300

    Modify routing conflict in the app js component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,18 +5,31 @@ import Assignment from "../pages/Assignment";
 import Assesments from "../pages/Assesments";
 
 import Home from "../pages/Home";
+
 import QuizPage from "../pages/QuizPage";
 import StudentDashboard from "../pages/StudentDashboard";
+
+import List from "../pages/List";
+import Single from "../pages/Single";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/">
-          <Route index element={<Assignment />} />
+          <Route index element={<Home />} />
+          <Route path="student">
+            <Route index element={<StudentDashboard />} />
+            <Route path="quiz" element={<QuizPage />} />
+            <Route path="assignment" element={<Assignment />} />
+          </Route>
         </Route>
-        <Route path="/assesments">
+        <Route path="mentor">
           <Route index element={<Assesments />} />
+          <Route path="grades">
+            <Route index element={<List />} />
+            <Route path=":studentId" element={<Single />} />
+          </Route>
         </Route>
       </Routes>
     </Router>
