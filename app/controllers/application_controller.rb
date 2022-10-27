b78bf53863b40053d4dc1d75ee444e93cc4cb3cc
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-10-27 10:31:55 +0300

    chore: refactor the authorization method to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,9 @@ class ApplicationController < ActionController::API
   
   before_action :authorize
   skip_before_action :authorize ,only: [:create]
+
+  def authorize
+    return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+  end
   
 end
