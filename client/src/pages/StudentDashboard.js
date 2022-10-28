15794771a92f74c2956c1efbaefe1491c4a5bd9a
Kimani <elvis.kimani@student.moringaschool.com>
2022-10-28 20:52:46 +0300

    Add home and assessment pages and styling

--- client/src/pages/StudentDashboard.js
+++ client/src/pages/StudentDashboard.js
@@ -3,6 +3,7 @@ import React from 'react'
 import StudentSideBar from '../components/StudentSideBar'
 import studentDashboard from "../CSS/_studentDashboard.scss";
 import { BsFillBellFill } from 'react-icons/bs';
+import { MdGrade } from 'react-icons/md';
 
 function StudentDashboard() {
   return (
@@ -14,9 +15,65 @@ function StudentDashboard() {
         </div>
         <div className='dashboarD'>
           <div className='notI'>
-            <h2>Notifications <BsFillBellFill className='n-icon'/></h2>
+            <h2>Notifications <BsFillBellFill className='n-icon'/> </h2>
+            <div className='notiContainer'>
+
+              <div className='notificatioN'>
+                <span className='titlE'>Course invitation</span>
+                <p>You have been invited to undertake CSS assessment</p>
+                <div className='nAv'>
+                  <span className='accepT'>Accept</span>
+                  <span className='declinE'>Decline</span>
+                </div>
+              </div>
+
+              <div className='notificatioN'>
+                <span className='titlE'>Course invitation</span>
+                <p>You have been invited to undertake java assessment</p>
+                <div className='nAv'>
+                  <span className='accepT'>Accept</span>
+                  <span className='declinE'>Decline</span>
+                </div>
+              </div>
+
+              <div className='notificatioN'>
+                <span className='titlE'>Course invitation</span>
+                <p>You have been invited to undertake HTML assessment</p>
+                <div className='nAv'>
+                  <span className='accepT'>Accept</span>
+                  <span className='declinE'>Decline</span>
+                </div>
+              </div>
+
+            </div>
           </div>
-          <div className='gradE'>Grades</div>
+          <div className='gradE'>
+            <h2>Grades <MdGrade className='n-icon'/></h2>
+            <div className='headinG'>
+              <span>ASSESSMENT NAME</span>
+              <span>DUE DATE</span>
+              <span>SCORE</span>
+            </div>
+            <div className='gradeCon'>
+              <div className='grADE'>
+                <span>Assessment one</span>
+                <span>08/05/22</span>
+                <span>70%</span>
+              </div>
+              <div className='grADE'>
+                <span>Assessment two</span>
+                <span>12/05/22</span>
+                <span>40%</span>
+              </div>
+              <div className='grADE'>
+                <span>Assessment three</span>
+                <span>07/05/22</span>
+                <span>80%</span>
+              </div>
+              
+              
+            </div>
+          </div>
         </div>
       </div>
       
