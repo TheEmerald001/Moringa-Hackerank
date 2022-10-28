dce3818868f8bfcf1f40d0e99406f8363b40a41c
Kimani <elvis.kimani@student.moringaschool.com>
2022-10-28 16:04:46 +0300

    style student Assignment tab

--- client/src/pages/Assignment.js
+++ client/src/pages/Assignment.js
@@ -1,25 +1,53 @@
 import React from 'react'
 import StudentSideBar from '../components/StudentSideBar'
+import assignment from '../CSS/_studentAssignments.scss'
+import { GiSiren } from 'react-icons/gi';
 
 
 function Assignment() {
   return (
-    <div className='asses'>
-      <StudentSideBar/>
-      <div className='content'>
-        <div>
-           <h2>Notice</h2>
-           <h3>Don't be late</h3>
-           <h5>No exceptions will be allowed</h5>
+    <div className="asses">
+      <StudentSideBar />
+      <div className="content">
+        <div className="A-title">
+          <h1>Assessments</h1>
         </div>
-        <div>
-          map assignments here
+        <div className="topContainer">
+          <div className="nOTICE">
+            <h2>Notice <GiSiren className='siren'/></h2>
+            <h4>Don't be late</h4>
+            <h5>No exceptions will be allowed</h5>
+          </div>
+          <div className='searchbaR'>
+            <input type='text' placeholder='Search Assessment'/>
+          </div>
         </div>
+        <div className='labeL'>
+          <h1>ASSESSMENTS</h1>
+        </div>
+        <div className='assesContainer'>
+          <div className='iteM'>
+            <h5>Assesment one</h5>
+            <span>Due to 12/10/22</span>
+            <span className='view'>View</span>
+          </div>
 
+          <div className='iteM'>
+            <h5>Assesment one</h5>
+            <span>Due to 12/10/22</span>
+            <span className='view'>View</span>
+          </div>
+
+          <div className='iteM'>
+            <h5>Assesment one</h5>
+            <span>Due to 12/10/22</span>
+            <span className='view'>View</span>
+          </div>
+          
+        </div>
       </div>
-
     </div>
-  )
+  );
 }
 
 export default Assignment
