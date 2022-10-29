5fb65e9c7fbdde78b974b18078910b19d7bf119f
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-10-29 14:06:04 +0300

    chore: commented out password regex for later use

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,9 +5,10 @@ class Student < ApplicationRecord
     has_many :assessments,through: :invites
     has_many :attempts
     has_one :studentprofile
+    has_one :total
 
     validates :username, presence:true, uniqueness: true
-    # validates :password, presence:true, length:{minimum: 6}
+    validates :password, presence:true, length:{minimum: 6}
     # validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*(_|[^\w])).+\z/, message: "must contain at least one uppercase letter, one lowercase letter, one number, and one special character" }
     validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, message: "please use the correct email format"}
 end
