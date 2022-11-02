1fefa49a1271ecbea884052c1d5b30a3d0b788b9
Maurice <mauricenganga41@gmail.com>
2022-11-02 08:46:03 +0300

    Use success component to display message

--- client/src/components/CreateQuiz.js
+++ client/src/components/CreateQuiz.js
@@ -1,16 +1,15 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 import styled from "styled-components";
-import request from "../Helpers/requestMethods";
 
 import Sidebar from "./Sidebar";
 
 const CreateQuiz = () => {
   const [correctAnswer, setCorrectAnswer] = useState("");
   const location = useLocation();
+  const [success, setSuccess] = useState(false);
   const assessmentId = location.pathname.split("/")[3];
   const [assessment, setAssessment] = useState({});
 
@@ -47,6 +46,13 @@ const CreateQuiz = () => {
 
     try {
       const { data } = await axios.post("/mcqs", quizData);
+      setSuccess((success) => !success);
+      setFormData({
+        question: "",
+        answer1: "",
+        answer2: "",
+        answer3: "",
+      });
     } catch (error) {
       console.log(error);
     }
@@ -65,60 +71,77 @@ const CreateQuiz = () => {
         </Top>
 
         <Bottom>
-          <BottomForm onSubmit={handleSubmit}>
-            <FormInput>
-              <Label htmlFor="question">Question</Label>
-              <Input
-                id="question"
-                type="text"
-                name="question"
-                onChange={handleChange}
-                placeholder="Question"
-              />
-            </FormInput>
-            <FormInput>
-              <Label htmlFor="answer1">Asnwer 1</Label>
-              <Input
-                id="answer1"
-                type="text"
-                name="answer1"
-                onChange={handleChange}
-                placeholder="Answer One"
-              />
-            </FormInput>
-            <FormInput>
-              <Label htmlFor="answer2">Answer 2</Label>
-              <Input
-                id="answer2"
-                type="text"
-                name="answer2"
-                onChange={handleChange}
-                placeholder="Answer Two"
-              />
-            </FormInput>
-            <FormInput>
-              <Label htmlFor="answer3">Answer 3</Label>
-              <Input
-                id="answer3"
-                type="text"
-                name="answer3"
-                onChange={handleChange}
-                placeholder="Answer Three"
-              />
-            </FormInput>
-            <FormInput>
-              <Label htmlFor="correct">Correct Answer</Label>
-              <Input
-                id="correct"
-                type="text"
-                name="correct_answer"
-                onChange={(e) => setCorrectAnswer(e.target.value)}
-                placeholder="Correct Answer"
-              />
-            </FormInput>
+          {!success && (
+            <BottomForm onSubmit={handleSubmit}>
+              <FormInput>
+                <Label htmlFor="question">Question</Label>
+                <Input
+                  id="question"
+                  type="text"
+                  name="question"
+                  onChange={handleChange}
+                  placeholder="Question"
+                />
+              </FormInput>
+              <FormInput>
+                <Label htmlFor="answer1">Asnwer 1</Label>
+                <Input
+                  id="answer1"
+                  type="text"
+                  name="answer1"
+                  onChange={handleChange}
+                  placeholder="Answer One"
+                />
+              </FormInput>
+              <FormInput>
+                <Label htmlFor="answer2">Answer 2</Label>
+                <Input
+                  id="answer2"
+                  type="text"
+                  name="answer2"
+                  onChange={handleChange}
+                  placeholder="Answer Two"
+                />
+              </FormInput>
+              <FormInput>
+                <Label htmlFor="answer3">Answer 3</Label>
+                <Input
+                  id="answer3"
+                  type="text"
+                  name="answer3"
+                  onChange={handleChange}
+                  placeholder="Answer Three"
+                />
+              </FormInput>
+              <FormInput>
+                <Label htmlFor="correct_answer">Correct Answer</Label>
+                <Input
+                  id="correct_answer"
+                  type="text"
+                  name="correct_answer"
+                  onChange={(e) => setCorrectAnswer(e.target.value)}
+                  placeholder="Correct Answer"
+                />
+              </FormInput>
 
-            <Button>Submit</Button>
-          </BottomForm>
+              <Button>Submit</Button>
+            </BottomForm>
+          )}
+          {success && (
+            <MessageWrapper>
+              <Message>
+                Action has been performed successfully, send{" "}
+                <MessageButton onClick={() => setSuccess(!success)}>
+                  another
+                </MessageButton>{" "}
+                or go{" "}
+                <Link to="/mentors/assessments">
+                  <MessageButton>back</MessageButton>
+                </Link>{" "}
+                to assessments?
+              </Message>
+            </MessageWrapper>
+          )}
         </Bottom>
       </Wrapper>
     </Container>
@@ -207,4 +230,30 @@ const Button = styled.button`
   font-weight: 600;
   cursor: pointer;
   margin-top: 10px;
+`;
+
+const MessageWrapper = styled.main`
+  font-size: 1rem;
+  margin: 1.25rem;
+`;
+
+const Message = styled.div`
+  padding: 1.25rem;
+  border: 1px solid teal;
+  border-radius: 5px;
+`;
+
+const MessageButton = styled.span`
+  font-weight: 600;
+  cursor: pointer;
+  border-bottom: 1px solid #101f3c;
+  &:last-child {
+    color: #ea501a;
+    border-bottom: 1px solid #ea501a;
+  }
+  &:hover {
+    font-size: 0.8rem;
+    font-weight: 700;
+    transform: scale(1.1 1.5s ease);
+  }
 `;
