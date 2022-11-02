d9e341609681461509041b44d06ccc124969a62e
Kimani <elvis.kimani@student.moringaschool.com>
2022-11-02 18:03:17 +0300

    Fetch trial from codewar API

--- client/src/pages/Trial.js
+++ client/src/pages/Trial.js
@@ -1,12 +1,49 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import StudentSideBar from '../components/StudentSideBar'
+const url = 'https://www.codewars.com/api/v1/code-challenges/valid-braces'
 
 function Trial() {
+  const [data, setData] = useState()
+ 
+
+
+  useEffect(() =>{
+    axios.get(url).then((response) => {
+      setData(response.data)
+    }).catch((error) => {
+      console.log(error)
+    })
+  },[])
+
+  console.log(data)
+
   return (
     <div className='asses'>
         <StudentSideBar/>
         <div className='content'>
-            Trial from codewar
+        <div className="A-title">
+          <h1>KATA SAMPLE</h1>
+        </div>
+        <div className='triContainer'>
+          <div >
+            {data?.languages.map((lang)=>(
+              <li className='lang' >{lang}</li>
+            ))}
+          </div>
+          <div className='tags'>TAGS:[{data?.tags}]</div>
+          <div className='sample'>QUESTION</div>
+          <div className='question'>{data?.description}</div>
+          <div>
+            <textarea className='textarea' type='text' rows="20" cols="150"/>
+          </div>
+          <div className='buTt'>
+            <span className='trBUT'>SUBMIT</span>
+          </div>
+        </div>
+
+          
+            
         </div>
 
     </div>
@@ -14,3 +51,12 @@ function Trial() {
 }
 
 export default Trial
+;
+
+
+
+
+
+  
+
+  
